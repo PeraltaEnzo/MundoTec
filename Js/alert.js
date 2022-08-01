48a1a9a952af54631e3f41ccb3b0ca9a803b6e3c
Peralta Enzo <enzomatiascarp@gmail.com>
2022-08-01 18:46:32 -0300

    MundoTEC
    Trabajo final

---
+++ Js/alert.js
@@ -0,0 +1,13 @@
+Swal.fire({
+title:'bienvenido a MundoTEC!',
+text:'Permitir que nuestra pagina le envie todas las novedades?',
+backdrop:true,
+toast:true,
+position:'top-start',
+showConfirmButton:true,
+showDenyButton:true,
+confirmButtonText:'Permitir',
+denyButtonText:'Denegar',
+confirmButtonColor:'#23F73F',
+}
+  )
